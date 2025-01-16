6091dac0b1c92e316c5fa778599b22346503b751
noah-tournant <noah.tournant@supinfo.com>
2025-01-16 09:21:45 +0100

    Ajout des fichierset de la base de l'index.html et du js

---
+++ JS/index.js
@@ -0,0 +1,29 @@
+
+ document.querySelector('.voir-plus').addEventListener('mouseover', function() {
+    this.style.transform = 'scale(1.1)';
+    this.style.transition = 'transform 0.3s';
+});
+
+document.querySelector('.voir-plus').addEventListener('mouseout', function() {
+    this.style.transform = 'scale(1)';
+});
+
+// Animation pour le bouton "Rechercher un film"
+document.querySelector('.rechercher-film').addEventListener('click', function() {
+    this.innerText = 'Chargement...';
+    setTimeout(() => {
+        this.innerText = 'Je souhaite rechercher un film';
+    }, 2000);
+});
+
+// Animation pour les films
+document.querySelectorAll('.film').forEach(film => {
+    film.addEventListener('mouseover', () => {
+        film.style.transform = 'scale(1.05)';
+        film.style.transition = 'transform 0.3s';
+    });
+
+    film.addEventListener('mouseout', () => {
+        film.style.transform = 'scale(1)';
+    });
+});
